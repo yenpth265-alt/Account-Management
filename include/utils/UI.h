3f434ffa03d4a682c215b1b33f533af749e594a6
Hha-dhba <honganh0865@gmail.com>
2026-06-12 10:57:56 +0700

    Update UI.h

--- include/utils/UI.h
+++ include/utils/UI.h
@@ -1,1 +1,15 @@
 //khai báo các hàm vẽ menu console
+#ifndef UI_H
+#define UI_H
+
+// Các hàm hỗ trợ điều hướng
+void XoaManHinh();
+void DungManHinh();
+
+// Các hàm hiển thị menu
+void MenuChinh();       // Menu tổng quát
+void MenuQuanLy();      // Quản lý khách hàng và tài khoản
+void MenuGiaoDich();    // Nghiệp vụ nạp, rút, chuyển khoản
+void MenuBaoCao();      // Xem lịch sử giao dịch, sao kê
+
+#endif
