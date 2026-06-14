0de41929d09e460f0b9f1624ccafb730c5d9979f
Hha-dhba <honganh0865@gmail.com>
2026-06-14 18:11:31 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 # include <iostream>
 # include "utils/UI.h"
-# include "utils/file.Manager.h
+# include "utils/fileManager.h
+# include "include/services/BankSystem.h"
+# include "include/services/TransactionLogic.h"
 
 using namespace std;
+int main(){
+  // Khởi tạo đối tượng
+  BankSystem bank;
+  TransactionLogic trans(&bank);
+
+  // Nạp dữ liệu từ file vào hệ thống
+  LoadAllData(bank);
+
+  // Chạy giao diện
+  MenuChinh(bank, trans);
+
+  // Lưu dữ liệu lại vào file
+  SaveAllData(bank);
+
+  return 0;
+}
+  
