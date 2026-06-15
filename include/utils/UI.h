b547aad060615c569fd3018e918ed2856c4992aa
Hha-dhba <honganh0865@gmail.com>
2026-06-15 23:23:06 +0700

    Add files via upload

--- include/utils/UI.h
+++ include/utils/UI.h
@@ -4,16 +4,16 @@
 
 #include "../services/BankSystem.h"
 #include "../services/TransactionLogic.h"
-
+#include "../services/ReportLogic.h"
 // Các hàm hỗ trợ điều hướng
 void XoaManHinh();
 void DungManHinh();
 double NhapSoTien();
 
 // Các hàm hiển thị menu có truyền tham số hệ thống
-void MenuChinh(BankSystem& bank, TransactionLogic& trans);      // Menu tổng quát
-void MenuQuanLy(BankSystem& bank);                              // Quản lý khách hàng và tài khoản
-void MenuGiaoDich(TransactionLogic& trans);                     // Nghiệp vụ nạp, rút, chuyển khoản
-void MenuBaoCao(BankSystem& bank);                              // Xem lịch sử giao dịch, sao kê
+void MenuChinh(BankSystem& bank, TransactionLogic& trans, ReportLogic& report);      // Menu tổng quát
+void MenuQuanLy(BankSystem& bank);                                                   // Quản lý khách hàng và tài khoản
+void MenuGiaoDich(TransactionLogic& trans);                                          // Nghiệp vụ nạp, rút, chuyển khoản
+void MenuBaoCao(BankSystem& bank, ReportLogic& report);                              // Xem lịch sử giao dịch, sao kê
 
 #endif
